696bd15fd907deaf86b226133fbd92c9c020e912
Simon Perriard <simon.perriard@epfl.ch>
2020-03-03 21:06:58 +0100

    Cleaning

--- cs412_lab1/src/crc.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRC_H
-#define CRC_H
-
-unsigned long update_crc(unsigned long crc, unsigned char *buf,
-                        int len);
-
-unsigned long crc(unsigned char *buf, int len);
-
-#endif
